d1ef59413e1a62de4bcb5aaf72604fbe462afdb9
Hassan Alidoost <h.alidoost76@gmail.com>
2023-05-05 00:08:46 +0330

    Phase2

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -10,6 +10,11 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function index(){
+
+    }
+
+
     public function update(UpdateUserRequest $request , User $user){
         $user->update($request->validated());
         return response()->json(['message' => 'User updated successfully!' , 'user' => UserResource::make($user)]);
