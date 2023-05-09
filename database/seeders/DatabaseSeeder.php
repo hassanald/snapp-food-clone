273a325cf29b068f0ce783890af0643ce8d93b59
Hassan Alidoost <h.alidoost76@gmail.com>
2023-05-09 16:28:18 +0330

    Order statuses is added to db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 use App\Models\Category;
 use App\Models\Discount;
 use App\Models\Food;
+use App\Models\OrderStatus;
 use App\Models\Restaurant;
 use App\Models\Role;
 use Illuminate\Database\Seeder;
@@ -19,16 +20,35 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        Role::factory()->create(['title' => 'admin']);
-        Role::factory()->create(['title' => 'seller']);
-        Role::factory()->create(['title' => 'user']);
+//        Role::factory()->create(['title' => 'admin']);
+//        Role::factory()->create(['title' => 'seller']);
+//        Role::factory()->create(['title' => 'user']);
+//
+//         \App\Models\User::factory()->create([
+//             'name' => 'admin',
+//             'email' => 'admin@gmail.com',
+//             'phone' => '09212776349',
+//             'password' => bcrypt(12345678),
+//             'role_id' => 1,
+//         ]);
 
-         \App\Models\User::factory()->create([
-             'name' => 'admin',
-             'email' => 'admin@gmail.com',
-             'phone' => '09212776349',
-             'password' => bcrypt(12345678),
-             'role_id' => 1,
-         ]);
+        OrderStatus::create([
+            'title' => 'reject'
+        ]);
+        OrderStatus::create([
+            'title' => 'unpaid'
+        ]);
+        OrderStatus::create([
+            'title' => 'awaiting'
+        ]);
+        OrderStatus::create([
+            'title' => 'preparing'
+        ]);
+        OrderStatus::create([
+            'title' => 'delivering'
+        ]);
+        OrderStatus::create([
+            'title' => 'delivered'
+        ]);
     }
 }
