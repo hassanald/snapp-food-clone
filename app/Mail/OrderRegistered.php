2dcf5cbb9b5370d0d6ec551ad6d05eeab424c9f6
Hassan Alidoost <h.alidoost76@gmail.com>
2023-05-11 23:27:36 +0330

    Mail to queue is implemented

--- app/Mail/OrderRegistered.php
+++ app/Mail/OrderRegistered.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class OrderRegistered extends Mailable
+class OrderRegistered extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
